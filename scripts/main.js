5e063e82c3088a1907866a991f87e5b42f991ce4
sarah <148671144+sarahzhng@users.noreply.github.com>
2024-01-23 19:31:37 +0000

    update js

--- scripts/main.js
+++ scripts/main.js
@@ -1,38 +1,38 @@
 // adding image changer 
 
-const myImage = document.querySelector("img");
+const myImage = document.querySelector('img');
 
 myImage.oneclick = function() {
-    const mySrc = myImage.getAttribute("src");
-    if (mySrc === "image/firefox.png") {
-        myImage.setAttribute("src", "images/firefox2.png");
+    const mySrc = myImage.getAttribute('src');
+    if (mySrc === 'image/firefox.png') {
+        myImage.setAttribute('src', 'images/firefox2.png');
     } else {
-        myImage.setAttribute("src", "images/firefox.png")
+        myImage.setAttribute('src', 'images/firefox.png');
     }
 }
 
 // personalized welcome message 
 
-let myButton = document.querySelector("button");
-let myHeading = document.querySelector("h1"); 
+let myButton = document.querySelector('button');
+let myHeading = document.querySelector('h1'); 
 
 // create function to set peresonalized greeting 
 function setUserName(){
-    const myName = prompt("please enter your name:");
+    const myName = prompt('please enter your name:');
     if (!myName){
         setUserName();
     } else {
-        localStorage.setItem("name", myName);
-        myHeading.innerHTML = `Mozilla is cool, ${myName}`;
+        localStorage.setItem('name', myName);
+        myHeading.innerHTML = 'Mozilla is cool, ' + myName;
     }
 }
 
 // conditional block that initalizes the page 
-if (!localStorage.getItem("name")) {
+if (!localStorage.getItem('name')) {
     setUserName();
 } else {
-    const storedName = localStorage.getItem("name");
-    myHeading.innerHTML = `Mozilla is cool ${storedName}`;
+    const storedName = localStorage.getItem('name');
+    myHeading.innerHTML = 'Mozilla is cool, ' + storedName;
 }
 
 // onclick button to enter a different name 
