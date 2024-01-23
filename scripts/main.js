6e4271d0dfc75acf14fb028d51aaae4f6febae8e
sarah <148671144+sarahzhng@users.noreply.github.com>
2024-01-23 22:41:56 +0000

    query user

--- scripts/main.js
+++ scripts/main.js
@@ -8,26 +8,11 @@ function show() {
   }
 
   function setUserName() {
-    // ask user for name
     var userName = prompt("Please enter your name:");
     if (userName) {
       localStorage.setItem('name', userName);
       document.getElementById('myHeading').innerHTML = 'Hello! Nice to Meet you, ' + userName;
     }
   }
-  
-  // Conditional block to initialize the page
-  if (!localStorage.getItem('name')) {
-    // Call the setUserName function if the name is not stored
-    setUserName();
-  } else {
-    // Retrieve the stored name and display a greeting
-    var storedName = localStorage.getItem('name');
-    document.getElementById('myHeading').innerHTML = 'Hello! Nice to Meet you, ' + storedName;
-  }
 
-// onclick button to enter a different name 
-myButton.onclick = function() {
-    setUserName();
-  }
 
