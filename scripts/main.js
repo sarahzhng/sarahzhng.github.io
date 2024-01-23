27ac4dcf14ee6fbf0cf5d0e0c95b278e7e852c01
sarah <148671144+sarahzhng@users.noreply.github.com>
2024-01-23 19:09:31 +0000

    js update

---
+++ scripts/main.js
@@ -0,0 +1,42 @@
+// adding image changer 
+
+const myImage = document.querySelector("img");
+
+myImage.oneclick = () => {
+    const mySrc = myImage.getAttribute("src");
+    if (mySrc === "image/firefox.png") {
+        myImage.setAttribute("src", "images/firefox2.png");
+    } else {
+        myImage.setAttribute("src", "images/firefox.png")
+    }
+}
+
+// personalized welcome message 
+
+let myButton = document.querySelector("button");
+let myHeading = document.querySelector("h1"); 
+
+// create function to set peresonalized greeting 
+function setUserName(){
+    const myName = prompt("please enter your name:");
+    if (!myName){
+        setUserName();
+    } else {
+        localStorage.setItem("name", myName);
+        myHeading.textContent = `Mozilla is cool, ${myName}`;
+    }
+}
+
+// conditional block that initalizes the page 
+if (!localStorage.getItem("name")) {
+    setUserName();
+} else {
+    const storedName = localStorage.getItem("name");
+    myHeading.textContent = `Mozilla is cool ${storedName}`;
+}
+
+// onclick button to enter a different name 
+myButton.oneclick = () => {
+    setUserName();
+};
+
