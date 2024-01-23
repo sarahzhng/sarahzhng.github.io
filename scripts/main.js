0c0745d3b9421746a8580fae7278e471ce559882
sarah <148671144+sarahzhng@users.noreply.github.com>
2024-01-23 14:45:04 -0500

    Update main.js
    Signed-off-by: sarah  <148671144+sarahzhng@users.noreply.github.com>

--- scripts/main.js
+++ scripts/main.js
@@ -3,14 +3,14 @@
 const myImage = document.querySelector('img');
 
 myImage.onclick = function() {
-    let mySrc = myImage.getAttribute('src');
-    if(mySrc === 'images/firefox.png') {
-      myImage.setAttribute ('src','images/firefox2.png');
-    } else {
-      myImage.setAttribute ('src','images/firefox.png');
-    }
+  let mySrc = myImage.getAttribute('src');
+  if(mySrc === 'images/firefox.png') {
+    myImage.setAttribute ('src','images/firefox2.png');
+  } else {
+    myImage.setAttribute ('src','images/firefox.png');
   }
-  
+}
+
 // personalized welcome message 
 let myButton = document.querySelector('button');
 let myHeading = document.querySelector('h1'); 
