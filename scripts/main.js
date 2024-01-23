ce16f97f71f2b0519d50a3799c1dc4352ae69354
sarah <148671144+sarahzhng@users.noreply.github.com>
2024-01-23 22:27:57 +0000

    update button js and index

--- scripts/main.js
+++ scripts/main.js
@@ -3,10 +3,8 @@ let myHeading = document.querySelector('h2');
 
 // create function to set show photo
 function show() {
-    var sourceimage = "images/cat.jpeg";
     var img = document.getElementById ('image')
-    img.src = sourceOfPicture.replace('90x90', '200x200');
-    img.style.display = "block"
+    img.style.display = (img.style.display === 'none') ? 'block' : 'none';
   }
 
 // conditional block that initalizes the page 
@@ -14,7 +12,7 @@ if(!localStorage.getItem('name')) {
     setUserName();
   } else {
     let storedName = localStorage.getItem('name');
-    myHeading.innerHTML = 'Hello! Nice to Meet you!' + storedName;
+    myHeading.innerHTML = 'Hello! Nice to Meet you! ' + storedName;
   }
 
 // onclick button to enter a different name 
